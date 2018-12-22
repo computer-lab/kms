5620a05597f04ff3e0a227f2f89a1efb2efd578b
Patrick Steadman <ptsteadman@gmail.com>
2018-12-22 18:48:52 -0500

    remove unnecessary console.log

--- kms.js
+++ kms.js
@@ -8,7 +8,6 @@ deactivateAllMembersButAdmins = async (dryRun = true) => {
   const { bootData: { api_token }, adminMembers: { members }} = TS.redux.getState()
   const shouldDeactivate = m => !m.is_admin && !m.is_bot && !m.is_primary_owner && !m.is_owner
   const membersToDeactivate = members.filter(shouldDeactivate)
-  console.log('membersToDeactivate:', membersToDeactivate);
   console.log(`About to deactivate ${membersToDeactivate.length} members.`)
   const membersToSave = members.filter(m => !shouldDeactivate(m))
   console.log(`These members will not be deactivated: ${membersToSave.map(m => m.name).join(' ')}`)
