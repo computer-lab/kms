e080714b6380c865d116abc9b7d6e44d16d35a23
Patrick Steadman <ptsteadman@gmail.com>
2018-12-22 17:50:57 -0500

    wip timeout loop over members

--- kms.js
+++ kms.js
@@ -1,27 +1,28 @@
 // KMS (Kill My Slack)
 // This function had the intended effect when pasted into the browser console on a "Manage members" page (`https://your-workspace.slack.com/admin'`) and executed on xx/xx/xxxx.
 
-deactivateAllMembersButAdmins = (dryRun = true) => {
+async deactivateAllMembersButAdmins = (dryRun = true) => {
   const { bootData: { api_token }, adminMembers: { members }} = TS.redux.getState()
   const shouldDeactivate = m => !m.is_admin && !m.is_bot && !m.is_primary_owner && !m.is_owner
   const membersToDeactivate = members.filter(shouldDeactivate)
+  console.log('membersToDeactivate:', membersToDeactivate);
   console.log(`About to deactivate ${membersToDeactivate.length} members.`)
   const membersToSave = members.filter(m => !shouldDeactivate(m))
   console.log(`These members will not be deactivated: ${membersToSave.map(m => m.name).join(' ')}`)
-
-  const TEST_UID = 'U0J6BDWMQ'
-
-  const formData = new FormData()
-  formData.append('user', TEST_UID)
-  formData.append('token', api_token)
-  formData.append('_x_mode', 'online')
-
-  const setMemberInactive = uid => fetch('/api/users.admin.setInactive', {
-    body: formData,
-    method: 'POST',
-  });
-
-  setMemberInactive(TEST_UID)
+  const setMemberInactive = m => {
+    const formData = new FormData()
+    formData.append('user', m.id)
+    formData.append('token', api_token)
+    formData.append('_x_mode', 'online')
+    return fetch('/api/users.admin.setInactive', {
+      body: formData,
+      method: 'POST',
+    });
+  }
+  const pretendToSetMemberInactive = m => console.log(`Pretending to deactivate ${m.id}`)
+  const thingToDo = dryRun ? pretendToSetMemberInactive : setMemberInactive
+  const timer = ms => Promise(res => setTimeout(res, ms))
+  membersToDeactivate.ForEach(m => await timer(thingToDo))
 }
 
 deactivateAllMembersButAdmins()
